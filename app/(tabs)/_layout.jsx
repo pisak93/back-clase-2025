5bc4f576b25752341694190202bf3db007a7cabd
pisak93 <sebastian.marquez93@hotmail.com>
2026-03-27 00:27:27 -0500

    Cambio icono Home

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,4 +1,5 @@
-import { Stack, Tabs } from "expo-router";
+import { Feather } from '@expo/vector-icons';
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
   return(
@@ -13,9 +14,22 @@ export default function RootLayout() {
       },
       headerStyle:{
         backgroundColor:"#f47b7b"
+      },
+      tabBarStyle:{
+        backgroundColor:"#3B0505",
+        height:70,
+        paddingTop:10
+      },
+      tabBarIconStyle:{
+        backgroundColor:"#f47b7b",
+        width:45,
+        height:45,
+        borderRadius:100
       }
+    
     }}>
-<Tabs.Screen name="index" options={{headerTitle:"Presente"}} />
+<Tabs.Screen name="index" options={{headerTitle:"Presente", tabBarShowLabel:false,
+    tabBarIcon: function(){return <Feather name="home" size={28} color={"#3B0505"} /> } }} />
 <Tabs.Screen name="futuro" options={{headerTitle:"Futuro",href:null}} />
     </Tabs>
   )
